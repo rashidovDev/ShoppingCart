3456afe2dc072f7e669aed42f83cbe81cbd62088
rashidovDev <anvarrashidov17@gmail.com>
2022-07-28 19:38:51 +0500

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+import { BsCart2 } from 'react-icons/bs';
+import { useShoppingCart } from "../context/ShoppingCartContext";
+
+
+export function Navbar() {
+    const { openCart, cartQuantity } = useShoppingCart()
+
+    return (
+        <NavbarBs sticky="top" className="nav shadow-sm mb-3 ">
+            <Container>
+                <Nav className="me-auto p-3">
+                    <Nav.Link to="/" as={NavLink} className="text-light">Home</Nav.Link>
+                    <Nav.Link to="/about" as={NavLink} className="text-light">About</Nav.Link>
+                    <Nav.Link to="/store" as={NavLink} className="text-light">Store</Nav.Link>
+                </Nav>
+                {cartQuantity > 0 && (
+                    <button
+                        onClick={openCart}
+                        style={{ width: "3rem", height: "3rem", position: "relative" }} className="btn btn-primary rounded-circle">
+                        <BsCart2 className="fs-3 text-center" />
+                        <div className="rounded-circle bg-light d-flex justify-content-center align-items-center text-danger"
+                            style={{ color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%,25%)" }}
+                        >
+                            {cartQuantity}
+                        </div>
+                    </button>
+                )}
+
+            </Container>
+        </NavbarBs>
+    )
+}
